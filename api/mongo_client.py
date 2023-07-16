eac11b1476b0510a2e118bc9c6abe926a386e45a
liuyiyang062 <withylyy@gmail.com>
2023-07-17 01:52:42 +0800

    final

--- api/mongo_client.py
+++ api/mongo_client.py
@@ -4,19 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env.local")
 
-
 MONGO_URL = os.environ.get("MONGO_URL", "mongo")
-MONGO_USERNAME = os.environ.get("MONGO_USENAME", "root")
+MONGO_USERNAME = os.environ.get("MONGO_USERNAME", "root")
 MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD", "")
 MONGO_PORT = os.environ.get("MONGO_PORT", 27017)
 
 mongo_client = MongoClient(
-    host=MONGO_URL, username=MONGO_USERNAME, password=MONGO_PASSWORD, port=MONGO_PORT
+    host=MONGO_URL,
+    username=MONGO_USERNAME,
+    password=MONGO_PASSWORD,
+    port=MONGO_PORT,
 )
 
 
 def insert_test_document():
+    """Inserts sample document to the test_collection in the test db"""
     db = mongo_client.test
-    test_colllecttion = db.test_collection
-    res = test_colllecttion.insert_one({"name": "Bogdan", "instructor": True})
+    test_collection = db.test_collection
+    res = test_collection.insert_one({"name": "Bogdan", "instructor": True})
     print(res)
